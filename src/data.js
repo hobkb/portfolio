7159b9331b7097771fbd74c4f1b4a89f8967c44c
hobkb <sarthakkhadka12345@gmail.com>
2024-07-19 16:29:44 +0545

    added portfolio

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    name: "hobkb",
+    githubLink: "https://github.com/hobkb" ,
+    email : "sarthakkhadka12345@gmail.com",
+    hobbies : [
+        "Movies",
+        "Travelling",
+        "Football"
+    ],
+    jobTitle : "Software Engineer",
+    degree: "Bachelor's in Computer Engineering",
+    campus: "Pulchowk Campus",
+    location: "Nepal",
+    spokenLanguages: "Nepali,English",
+}
+export default data;
